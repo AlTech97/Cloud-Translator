295ec1bd71852f26ab636deef9fe2a4c1d527275
ALFONSO INGENITO <a.ingenito32@studenti.unisa.it>
2020-02-11 16:39:01 +0100

    Implementata la funzione di Login con redirect al traduttore
    Implementare il passaggio di username e password alla classe MainWindow in caso di successo
    Inserire i puntini al posto delle lettere nel campo password della form

--- Downloads/traduttore/traduttore/mio traduttore 2/mio traduttore 2/Login.xaml.cs
+++ Downloads/traduttore/traduttore/mio traduttore 2/mio traduttore 2/Login.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Navigation;
+using System.Data.SqlClient;
 
 namespace mio_traduttore_2
 {
@@ -19,13 +21,93 @@ namespace mio_traduttore_2
     /// </summary>
     public partial class Window1 : Window
     {
+        string pswd, usr;
+        int i, j;
+
         public Window1()
         {
             InitializeComponent();
         }
 
+        //salva l'email inserita nella form in una variabile
+        private void EmailText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            usr = sender.ToString();
+            j = usr.IndexOf(' ');  //trova l'occorrenza dello spazio nella stringa
+            j++;
+            usr = usr.Substring(j);   //usr ora contiene solo l'email dello user
+
+        }
+
+        //salva la pass inserita nella form in una variabile
+        private void PasswordText_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            pswd = sender.ToString();
+            i = pswd.IndexOf(' ');  //trova l'occorrenza dello spazio nella stringa
+            i++;                       //incremento i per portarlo al valore che ci serve
+            pswd = pswd.Substring(i);   //pswd ora contiene solo la password
+        }
+
         private void LogIn(object sender, RoutedEventArgs e)
         {
+            string constr;
+
+            // for the connection to 
+            // sql server database 
+            SqlConnection conn;
+
+            // Data Source is the name of the 
+            // server on which the database is stored. 
+            // The Initial Catalog is used to specify 
+            // the name of the database 
+            // The UserID and Password are the credentials 
+            // required to connect to the database. 
+            constr = @"Server=tcp:traduttoredb.database.windows.net,1433;Initial Catalog=TraduttoreDB;Persist Security Info=False;
+                User ID=admintraduttore;Password=Settimana1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+
+            conn = new SqlConnection(constr);
+
+            conn.Open();
+
+            // use to perform read and write operations in the database 
+            SqlCommand cmd;
+
+            //use to read a row in table one by one
+            SqlDataReader dreader;
+
+            // to sore SQL command and the output of SQL command 
+            string sql, output = "";
+
+            // use to fetch rwos from demo table 
+            sql = "Select Email, Password from Utente";
+
+            // to execute the sql statement 
+            cmd = new SqlCommand(sql, conn);
+
+            // fetch all the rows from the demo table 
+            dreader = cmd.ExecuteReader();
+
+            // for one by one reading row 
+            while (dreader.Read())
+            {
+
+                if (dreader.GetValue(0).Equals(usr) && dreader.GetValue(1).Equals(pswd))
+                {
+                    Console.WriteLine("USERNAME E PASSWORD CORRETTI, FAI IL REDIRECT");
+                    Switch();
+                }
+            }
+
+            // to close all the objects 
+            dreader.Close();
+            cmd.Dispose();
+            conn.Close();
+
+            Console.WriteLine("USERNAME O PASSWORD ERRATI!");
+        }
+
+        private void Switch()
+        {       //PASSARE LE VARIABILI USR E PSWD AL MAIN WINDOW
             var win = new MainWindow();
             win.Show();
             this.Close();
